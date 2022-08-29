0ddfa93cf4a7bfe3442c8b9f5ba63d35085628f5
Nativ Levy <nlev245@gmail.com>
2022-08-29 12:03:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,15 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import yt_dlp
 
-"""
-# Welcome to Streamlit!
+st.write("# Download Video")
+url = st.text_input('The video URL (youtube)')
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+if st.button('Convert'):
+    filename = 'video'
+    ydl_opts = {'outtmpl': filename}  # 'f': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]'}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    with open(filename + '.mkv', 'rb') as f:
+        st.download_button('Download Video', f, 'video.mkv')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
