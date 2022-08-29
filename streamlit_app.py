41afb521f200620690124ffde7d68e6feabf1e72
Nativ Levy <nlev245@gmail.com>
2022-08-29 18:55:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,34 @@
 import os
+import glob
 
 import streamlit as st
 import yt_dlp
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 url = st.text_input('Video URL')
+if "&list" in url:
+    url = url.split("&list")[0]
 
-start_second = st.text_input('Start second')
-end_second = st.text_input('End second')
+start_second = st.text_input('Start minutes:seconds', '0:0')
+end_second = st.text_input('End minutes:seconds', '0:0')
 
-start_second = int(start_second) if start_second else 0
-end_second = int(end_second) if end_second else 0
+minutes, seconds = start_second.split(':')
+minutes_end, seconds_end = end_second.split(':')
+
+start_second = int(minutes) * 60 + int(seconds)
+end_second = int(minutes_end) * 60 + int(seconds_end)
+
+st.write("Start Second: " + str(start_second), "End Second: " + str(end_second))
 
 if st.button('Convert'):
-    filename = 'video.mkv'
-
-    # try:
-    #     open(filename, 'w').close()
-    # except Exception:
-    #     pass
-
-    ydl_opts = {'outtmpl': filename, '--no-continue': True}  # 'f': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4'}
-
-    # if the file exists, delete it
-    if os.path.exists(filename):
-        os.remove(filename)
+    ydl_opts = {}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+
+    list_of_files = glob.glob('*')  # * means all if need specific format then *.csv
+    filename = max(list_of_files, key=os.path.getctime)
+    print(filename)
 
     target_name = 'part.mp4'
 
@@ -37,5 +38,5 @@ if st.button('Convert'):
 
     with open(target_name, 'rb') as f:
         st.download_button('Download Video', f, target_name)
-        # os.remove(filename)
-        # os.remove(target_name)
+        os.remove(filename)
+        os.remove(target_name)
