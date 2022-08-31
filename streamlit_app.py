d000b98fdccbf0b72c10c0106cd57491c99e1d47
Nativ Levy <nlev245@gmail.com>
2022-08-31 13:15:17 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,11 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 
 url = st.text_input('Video URL')
 if "&list" in url:
-    url = url.split("&list")[0]
+    url = url.split("&")[0]
+
+# https://www.youtube.com/watch?v=m5t082lDFdc
+# 4:23
+# 10:26
 
 start_second = st.text_input('Start minutes:seconds', '0:0')
 end_second = st.text_input('End minutes:seconds', '0:0')
@@ -21,6 +25,14 @@ end_second = int(minutes_end) * 60 + int(seconds_end)
 st.write("Start Second: " + str(start_second), "End Second: " + str(end_second))
 
 if st.button('Convert'):
+    test = os.listdir('.')
+    for item in test:
+        if item.endswith(".webm") or item.endswith(".mp4") or item.endswith(".mkv") or item.endswith(".mp3"):
+            try:
+                os.remove(item)
+            except:
+                pass
+
     ydl_opts = {}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -29,7 +41,7 @@ if st.button('Convert'):
     list_of_files = glob.glob('*')  # * means all if need specific format then *.csv
     filename = max(list_of_files, key=os.path.getctime)
     print(filename)
-
+    
     target_name = 'part.mp4'
 
     with VideoFileClip(filename) as video:
@@ -38,5 +50,15 @@ if st.button('Convert'):
 
     with open(target_name, 'rb') as f:
         st.download_button('Download Video', f, target_name)
-        os.remove(filename)
-        os.remove(target_name)
+        try:
+            os.remove(filename)
+            os.remove(target_name)
+        except:
+            pass
+        test = os.listdir('.')
+        for item in test:
+            if item.endswith(".webm") or item.endswith(".mp4") or item.endswith(".mkv") or item.endswith(".mp3"):
+                try:
+                    os.remove(item)
+                except:
+                    pass
